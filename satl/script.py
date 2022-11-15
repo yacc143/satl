7902b433ebe818304b0ebabbb93d37296cb5127b
Andreas Kostyrka <yacc143@gmail.com>
2022-11-15 18:49:30 +0100

    basic infrastructure to deal with variables: base and the real ones representing each possible number in a field. The joys of coding integers as hot one in a bitmap.

--- satl/script.py
+++ satl/script.py
@@ -25,34 +25,62 @@ def run_limboole(limboole, formula):
     else:
         raise ValueError("No solution")
 
-def format_filter_result(res):
-    out = []
-    for n, v in sorted(res.items()):
-        if v:
-            out.append(f"!{n}")
-        else:
-            out.append(n)
-    return f"({' | '.join(out)})"
 
-def solutions(no, name, solutions):
-    output = []
-    out = output.append
-    out(f"name: {name}")
-    out(f"student number: {no}")
-    out(f"number of solutions: {len(solutions)}")
-    out("solutions:")
-    for sol in solutions:
-        out(format_filter_result(sol))
-    out("") # at least Unix text file should end with NL
-    return "\n".join(output)
+def variables_bases(size):
+    assert size % 2 == 0 and size >= 4 # only even size over 3 supported
+    number = size * size
+    width = len(str(number))
+    vars = [f"v{i:0{width}d}" for i in range(number)]
+    return vars
+
+def fields(size):
+    vars = variables_bases(size)
+    while vars:
+        row, vars = vars[:size], vars[size:]
+        yield row
+
+def variables_real(size):
+    bases = variables_bases(size)
+    width = len(str(size))
+    vars = {}
+    for v in bases:
+        l = [None, ]
+        for i in range(1, size + 1):
+            l.append(f"{v}{i:0{width}d}")
+        vars[v] = l
+    return vars
 
 @click.command()
-@click.option("--formulas", "-f", type=click.File(mode="rb"), default="./formulas.zip")
+@click.option("--size", "-s", type=int, default=4)
 @click.option("--limboole", default=LIMBOOLE)
 @click.argument("studentno")
-@click.argument("studentname")
-def main(formulas, limboole, studentno, studentname):
+@click.argument("assignments", nargs=-1)
+def main(size, limboole, studentno, assignments):
     selector = studentno[-2:]
+    if assignments:
+        vars = {}
+        for a in assignments:
+            # the error checking is rough. This is a student assignment. Shrugh.
+            assert "=" in a
+            k, v = a.split("=")
+            v = int(v)
+            assert 1 <= v <= size, "the value assigned are constrained by the size of the sudoku"
+            assert k in variables_bases(size)
+            vars[k] = v
+        print(vars)
+
+        real = variables_real(size)
+        print([real[k][v]for k, v in vars.items()])
+
+    else:
+        print(f"Sudoku Field of size {size}:")
+        print("-----------------------------")
+        print("")
+        for row in fields(size):
+            print(" ".join(row))
+        print("")
+        print("base variable names used for assignments in this program.")
+    return
     with zipfile.ZipFile(formulas, "r") as zf:
         formula = zf.read(f"formulas/{selector}.limboole").decode("ascii")
     print(formula)
