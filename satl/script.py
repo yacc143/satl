a340a45409d8cc155c35d48391022a8008b9addd
Andreas Kostyrka <yacc143@gmail.com>
2022-11-15 21:32:00 +0100

    rules for maximum 1 number per field and forcing the 1,2,3,4 set on a random field of variables.

--- satl/script.py
+++ satl/script.py
@@ -3,6 +3,7 @@ import subprocess
 import tempfile
 import platform
 import zipfile
+import itertools
 
 LIMBOOLE={
     "Windows": "limboole.exe",
@@ -27,7 +28,7 @@ def run_limboole(limboole, formula):
 
 
 def variables_bases(size):
-    assert size % 2 == 0 and size >= 4 # only even size over 3 supported
+    assert size % 2 == 0 and size >= 4 # only even size over 3 supported, so: 4, 6
     number = size * size
     width = len(str(number))
     vars = [f"v{i:0{width}d}" for i in range(number)]
@@ -53,6 +54,8 @@ def variables_real(size):
 def force_at_least_one_number_per_field(size):
     """ return a partial formula that forces that at least one digit can be switched on in a field: 
     inspired by gc.pdf constraints item 1.
+
+    (Please remember, we learn proper citing rules only in the 3rd year.)
     """
     real = variables_real(size)
     output = []
@@ -62,7 +65,36 @@ def force_at_least_one_number_per_field(size):
     return " & ".join(output)
 
 
+def force_at_most_one_number_per_field(size):
+    """ return a partial formula that forces that at most one digit can be switched on in a field: 
+    inspired by gc.pdf constraints item 2.
+    """
+    real = variables_real(size)
+    output = []
+    for field_variables in real.values():
+        for var1, var2 in itertools.combinations(field_variables[1:], 2):
+            output.append(f"(!{var1} | !{var2})")
+    return " & ".join(output)
 
+def force_number_set(size, vars):
+    """force the numbers in the set of allowed numbers derived from the size of sudoku show up
+       at most once in the base variables given. Ideally the len(vars) == size
+       
+       Inspired by gc.pdf, the edge connection condition, roughly, because we are constructing our
+       edges between the variables in vars "on the fly" here.
+       """
+    assert len(vars) == size
+
+    output = []
+
+    real = variables_real(size)
+    for v1, v2 in itertools.combinations(vars, 2):
+        l1, l2 = real[v1], real[v2]
+        for num in range(1, size + 1):
+            output.append(f"(!{l1[num]}|!{l2[num]})")
+    return " & ".join(output)
+
+            
 
 @click.command()
 @click.option("--size", "-s", type=int, default=4)
@@ -71,6 +103,8 @@ def force_at_least_one_number_per_field(size):
 @click.argument("assignments", nargs=-1)
 def main(size, limboole, studentno, assignments):
     selector = studentno[-2:]
+    if size not in (4, 6):
+        raise ValueError("unsupported size: use 4 or 6")
     if assignments:
         vars = {}
         for a in assignments:
@@ -87,6 +121,8 @@ def main(size, limboole, studentno, assignments):
         print([real[k][v]for k, v in vars.items()])
 
         print(force_at_least_one_number_per_field(size))
+        print(force_at_most_one_number_per_field(size))
+        print(force_number_set(size, variables_bases(size)[:size]))
     else:
         print(f"Sudoku Field of size {size}:")
         print("-----------------------------")
